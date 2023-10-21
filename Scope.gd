cccacccb93552ac8d274b82b501ea4e2397ce95e
Bruno Silveira <dasilveira.bru@terra.com.br>
2023-10-21 05:27:25 -0300

    quase com a bola

--- Scope.gd
+++ Scope.gd
@@ -1,27 +1,37 @@
 extends Node2D
 
-@export var MOUSE_SPEED = 1
+@export var MOUSE_SPEED = 3
 @onready var marker = $Marker2D
-var teleported_mouse : bool = false
+var prev_mouse_pos : Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+	#Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+	Input.warp_mouse(get_viewport_rect().size/2)
+	prev_mouse_pos = get_global_mouse_position()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	var mouse_pos : Vector2 = get_global_mouse_position()
+	print_debug("MPOS: " + str(mouse_pos))
+	var velocity : Vector2 = prev_mouse_pos - mouse_pos
+	print_debug(velocity)
+	marker.global_position += velocity * delta * MOUSE_SPEED
+	prev_mouse_pos = mouse_pos
 
 func _input(event):
+	return
 	if event is InputEventMouseMotion:
-		if teleported_mouse:
-			teleported_mouse = false
-			return
 		var velocity : Vector2 = event.velocity
 		velocity = velocity / 100
 		print_debug(velocity)
 		marker.global_position += velocity * MOUSE_SPEED
 		Input.warp_mouse(get_viewport_rect().size/2)
-		teleported_mouse = true
 		
+
+
+func _on_mouse_cage_mouse_shape_exited(shape_idx):
+	prev_mouse_pos = get_viewport_rect().size/2
+	Input.warp_mouse(get_viewport_rect().size/2)
+	
