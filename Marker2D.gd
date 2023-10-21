67796d0c2b6de7d387a2907a041c3a22dbf36d5e
Bruno Silveira <dasilveira.bru@terra.com.br>
2023-10-21 15:04:29 -0300

    ainda com too fast mouse

--- Marker2D.gd
+++ Marker2D.gd
@@ -1,11 +1,62 @@
 extends Marker2D
 
+@export var MOUSE_SPEED = 150
+@export var MOUSE_ACCEL = 200
+@export var MOUSE_FRICTION = 100
+
+const MOVING_TRESHOLD = 0.1
+
+var prev_mouse_pos : Vector2 = Vector2.ZERO
+var tpd : bool = false
+var velocity : Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+	center_mouse()
+	prev_mouse_pos = get_mouse_pos()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	if tpd:
+		prev_mouse_pos = get_mouse_pos()
+		tpd = false
+		return
+	var mouse_pos : Vector2 = get_mouse_pos()
+	var mouse_direction : Vector2 = mouse_pos - prev_mouse_pos
+	if mouse_direction.length() > MOVING_TRESHOLD:
+		print_debug("MOVING" + str(mouse_direction))
+		print_debug("MPOS" + str(mouse_pos))
+		print_debug("MPREV" + str(prev_mouse_pos))
+		mouse_direction = mouse_direction.normalized()
+		velocity += MOUSE_ACCEL * delta * mouse_direction
+		if velocity.length() > MOUSE_SPEED:
+			velocity = velocity.normalized() * MOUSE_SPEED
+	else:
+		var op_dir : Vector2 = velocity.normalized() * -1
+		velocity += MOUSE_FRICTION * delta * op_dir
+	global_position += velocity * delta
+	prev_mouse_pos = mouse_pos
+		
+func center_mouse():
+	Input.warp_mouse(get_viewport_rect().size / 2)
+
+func get_mouse_pos() -> Vector2:
+	return get_local_mouse_position()
+
+
+func _on_mouse_cage_mouse_shape_exited(shape_idx):
+	return
+	prev_mouse_pos = get_viewport_rect().size/2
+	center_mouse()
+	tpd = true
+	print_debug("TPCENTER")
+
+
+func _on_area_2d_mouse_shape_entered(shape_idx):
+	return
+	prev_mouse_pos = get_viewport_rect().size/2
+	center_mouse()
+	tpd = true
+	print_debug("TPCENTER")
