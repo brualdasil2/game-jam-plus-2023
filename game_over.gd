f7e83a685241d6677854b5348ad5ebb8298ec68b
Bruno Silveira <dasilveira.bru@terra.com.br>
2023-11-11 18:31:31 -0300

    all lights

--- game_over.gd
+++ game_over.gd
@@ -10,3 +10,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_animation_player_animation_finished(anim_name):
+	pass
+
+
+func _on_audio_stream_player_finished():
+	get_tree().change_scene_to_file("res://menu.tscn")
